41cbc26d03255aaea9cc733c8c723abc56f9228b
yulu <yulu8798@gmail.com>
2026-02-07 22:48:07 +0800

    Update button position

--- static/js/cookie-consent.js
+++ static/js/cookie-consent.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function() {
                 layout: 'bar inline',
                 position: 'bottom',
                 equalWeightButtons: true,
-                flipButtons: false
+                flipButtons: true
             },
             preferencesModal: {
                 layout: 'box'
